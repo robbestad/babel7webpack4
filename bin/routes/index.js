d6f42a7dfc4d32f8a4e7f635ab40ebd261e791b1
Sven Anders Robbestad <sven.anders.robbestad@acando.no>
2018-02-26 09:52:26 +0100

    Remove index from dist

--- bin/routes/index.js
+++ bin/routes/index.js
@@ -7,7 +7,7 @@ const router = new Router()
 function wrapper(config) {
 	return router.get("/", async ctx => {
 		console.log(console.log(`${path.join("dist")}`))
-		await send(ctx, `${path.join("/dist/index.html")}`)
+		await send(ctx, `${path.join("/src/index.html")}`)
 	})
 }
 
