dfe4d20ebf271f2442a2a3aaeda01870f6fe036e
Sven Anders Robbestad <sven.anders.robbestad@acando.no>
2018-02-26 11:52:04 +0100

    Use mjs

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 module.exports = {
   context: path.resolve(__dirname, "./src"),
-  entry:   [path.resolve(__dirname, "src/index.jsx")],
+  entry:   [path.resolve(__dirname, "src/index.mjs")],
   output:  {
     filename: "bundle.js",
     path:     path.resolve(__dirname, "dist")
