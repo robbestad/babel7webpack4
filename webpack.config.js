3cffb5ae84856c9b2561525d9fb329f7caeff705
Sven Anders Robbestad <sven.anders.robbestad@acando.no>
2018-02-28 19:51:41 +0100

    Refactor to dot js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 module.exports = {
   context: path.resolve(__dirname, "./src"),
-  entry:   [path.resolve(__dirname, "src/index.mjs")],
+  entry:   [path.resolve(__dirname, "src/index.js")],
   output:  {
     filename: "bundle.js",
     path:     path.resolve(__dirname, "dist")
